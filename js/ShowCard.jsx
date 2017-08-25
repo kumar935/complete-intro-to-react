0dd5af06df6815f2d7d63240f0fc42d65fb3f090
kumar935 <kanimesh.s@gmail.com>
2017-08-25 10:00:57 +0530

    minor changes, and this is until redux started

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,13 +1,13 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 // dropping propTypes because using flow now
 // import {string} from 'prop-types';
 
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-// Wrapped the wrapper with Link VVVVVVVVVVVVVVVVVVVVV
+// Wrapped the wrapper with Link VVVVVVVVVVVVVVVVVVVVV and ANY too
 const Wrapper = styled(Link: any)`   
   width: 32%;
   border: 2px solid #333;
@@ -25,6 +25,7 @@ const Image = styled.img`
   margin-right: 10px;
 `;
 
+/*
 const ShowCard = (props: { poster: string, title: string, year: string, description: string, imdbID: string }) => (
   
   <Wrapper className="show-card" to={`/details/${props.imdbID}`}>
@@ -36,8 +37,28 @@ const ShowCard = (props: { poster: string, title: string, year: string, descript
     </div>
   </Wrapper> 
   
-  
 )
+*/
+
+class ShowCard extends Component{
+  shouldComponentUpdate () {
+    return false;
+    // ^this says once it has rendered, never update it.
+  }
+  props: Show
+  render() {
+    return (
+      <Wrapper className="show-card" to={`/details/${this.props.imdbID}`}>
+        <Image src={`/public/img/posters/${this.props.poster}`} alt={`${this.props.title} Show Poster`}/>
+        <div>
+          <h3>{this.props.title}</h3>
+          <h4>({this.props.year})</h4>
+          <p>{this.props.description}</p> 
+        </div>
+      </Wrapper>     
+    )
+  }
+}
 
 /*
 ShowCard.propTypes = {
